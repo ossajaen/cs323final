482336e8cd5da593466a0b5f256890520bc5dcc7
ossajaen <83042344+ossajaen@users.noreply.github.com>
2021-04-22 15:28:16 -0700

    Create Home.java

---
+++ Home.java
@@ -0,0 +1,22 @@
+package com.example.deadfishapp;
+
+public class Home {
+    private int mX;
+    private int mY;
+
+    public int getX() {
+        return mX;
+    }
+
+    public void setX(int x) {
+        mX = x;
+    }
+
+    public int getY() {
+        return mY;
+    }
+
+    public void setY(int y) {
+        mY = y;
+    }
+}
