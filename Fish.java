7f3222a1adc550f1058c2a3919205c086f47b059
ossajaen <83042344+ossajaen@users.noreply.github.com>
2021-04-22 15:28:33 -0700

    Create Fish.java

---
+++ Fish.java
@@ -0,0 +1,36 @@
+public class Fish {
+    private int mX;
+    private int mY;
+    private int mVelocity;
+
+    public void setVelocity(int velocity) {
+        mVelocity = velocity;
+    }
+
+    public int getVelocity() {
+        return mVelocity;
+    }
+
+    public void setX(int x) {
+        mX = x;
+    }
+
+    public void setY(int y) {
+        mY = y;
+    }
+
+    public int getX() {
+        return mX;
+    }
+
+    public int getY() {
+        return mY;
+    }
+
+    public void move(int destinationX, int destinationY) {
+        int distX = destinationX - mX;
+        int distY = destinationY - mY;
+        mX += distX/mVelocity;
+        mY += distY/mVelocity;
+    }
+}
