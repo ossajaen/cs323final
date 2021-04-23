9003bc2ba46c09d20d41ef29723fd87e94cbee9e
jjtrauger <83063647+jjtrauger@users.noreply.github.com>
2021-04-23 02:13:43 -0700

    Create Treasure.java
    this is simply a replacement for Home.java

---
+++ Treasure.java
@@ -0,0 +1,24 @@
+package com.example.deadfishapp;
+
+public class Treasure {
+    //data members
+    private int mX;
+    private int mY;
+
+    //setters and getters for treasure chest
+    public int getX() {
+        return mX;
+    }
+
+    public void setX(int x) {
+        mX = x;
+    }
+
+    public int getY() {
+        return mY;
+    }
+
+    public void setY(int y) {
+        mY = y;
+    }
+}
